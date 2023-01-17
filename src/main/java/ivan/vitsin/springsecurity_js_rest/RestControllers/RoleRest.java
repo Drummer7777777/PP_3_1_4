25541b609c6fedb73ed218c5bb0a87c7669413f5
Ivan Vitsin <vanyavitsin@gmail.com>
2023-01-17 11:07:03 +0500

    Написал рест контроллеры и проверил их работу через постман (убрал старые контроллеры)

---
+++ src/main/java/ivan/vitsin/springsecurity_js_rest/RestControllers/RoleRest.java
@@ -0,0 +1,26 @@
+package ivan.vitsin.springsecurity_js_rest.RestControllers;
+
+import ivan.vitsin.springsecurity_js_rest.model.Role;
+import ivan.vitsin.springsecurity_js_rest.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.management.relation.RoleList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/role")
+public class RoleRest {
+
+    private final RoleService roleService;
+
+    public RoleRest(RoleService roleService) {
+        this.roleService = roleService;
+    }
+    @GetMapping
+    public List<Role> getAllRoles() {
+        return roleService.findAllRoles();
+    }
+}
